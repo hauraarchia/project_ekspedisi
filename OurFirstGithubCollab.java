f5586742d59a9ad83e370c2cb918ed1d79790201
hauraarchia <hauraarchia@gmail.com>
2023-09-13 13:26:29 +0700

    fitur isna

--- OurFirstGithubCollab.java
+++ OurFirstGithubCollab.java
@@ -4,5 +4,6 @@
 public class OurFirstGithubCollab {
     public static void main(String[] args){
     System.out.println("ini ceritanya fitur pertama sudah berjalan baik");
+    System.out.println("ini ceritanya fitur isna");
     }
 }
