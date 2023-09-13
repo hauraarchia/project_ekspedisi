717f4cbb2e84a6f525a8e1719a56853cb4b2cd08
hauraarchia <hauraarchia@gmail.com>
2023-09-13 13:30:05 +0700

    fitur luthfi

--- OurFirstGithubCollab.java
+++ OurFirstGithubCollab.java
@@ -5,5 +5,6 @@ public class OurFirstGithubCollab {
     public static void main(String[] args){
     System.out.println("ini ceritanya fitur pertama sudah berjalan baik");
     System.out.println("ini ceritanya fitur isna");
+    System.out.println("ini ceritanya fitur luthfi");
     }
 }
