4d0e4df382f732ee94841c776d58e52269ece564
hauraarchia <hauraarchia@gmail.com>
2023-09-13 13:22:05 +0700

    fitur 1

---
+++ OurFirstGithubCollab.java
@@ -0,0 +1,8 @@
+/**
+ * OurFirstGithubCollab
+ */
+public class OurFirstGithubCollab {
+    public static void main(String[] args){
+    System.out.println("ini ceritanya fitur pertama sudah berjalan baik");
+    }
+}
